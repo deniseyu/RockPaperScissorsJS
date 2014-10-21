aa8a4b4f43c81c0481d8a6d166ff09e25acf7486
Denise <yu.denise.d@gmail.com>
2014-10-21 18:11:37 +0100

    boolean struggles

--- src/Rps.js
+++ src/Rps.js
@@ -2,48 +2,59 @@ function RPS() {};
 
 function Rock() {
 	this.type = 'rock';
-	this.message = 'Rock crushes scissors!'
+	this.message = 'Computer wins!'
 };
 
 function Paper() {
 	this.type = 'paper';
-	this.message = 'Paper covers rock!';
+	this.message = 'Computer wins!';
 };
 
 function Scissors() {
  	this.type = 'scissors';
- 	this.message = 'Scissors cuts paper!';
+ 	this.message = 'Computer wins!';
+ };
+
+ function Lizard(){
+ 	this.type = 'lizard';
+ 	this.message = 'Computer wins!';
+ };
+
+ function Spock(){
+ 	this.type = 'spock';
+ 	this.message = 'Computer wins!';
  };
 
  function Draw() {
- 	this.type = 'draw'
- 	this.message = 'Draw!'
+ 	this.type = 'draw';
+ 	this.message = 'Draw!';
  };
 
  function PlayerChoice(my_type){
- 	this.type = my_type
+ 	this.type = my_type;
+ 	this.message = 'You win!'
  };
 
 RPS.prototype.versus = function(choiceOne, choiceTwo) {
  if(choiceOne.type === choiceTwo.type){
- 	return (new Draw)
+ 	return (new Draw);
  }
-	if((choiceOne.type === ('rock') && choiceTwo.type === ('scissors')) ||
-	  (choiceOne.type === ('scissors') && choiceTwo.type === ('rock'))) 
-	return (new Rock);
-
-	if((choiceOne.type === ('paper') && choiceTwo.type === ('scissors')) ||
-		(choiceOne.type === ('scissors') && choiceTwo.type === ('paper')))
-	return (new Scissors);
-
-	if((choiceOne.type === ('paper') && choiceTwo.type === ('rock')) ||
-		(choiceOne.type === ('rock') && choiceTwo.type === ('paper')))
-	return (new Paper);
+	if (	((choiceOne.type === 'rock')	&& ((choiceTwo.type === 'scissors') || (choiceTwo.type === 'lizard'))) ||
+			((choiceOne.type === 'scissors')&& ((choiceTwo.type === 'paper') || (choiceTwo.type === 'lizard'))) ||
+			((choiceOne.type === 'paper') 	&& ((choiceTwo.type === 'rock') || (choiceTwo.type === 'spock'))) ||
+			((choiceOne.type === 'lizard') 	&& ((choiceTwo.type === 'spock') || (choiceTwo.type === 'paper'))) ||
+			((choiceOne.type === 'spock') 	&& ((choiceTwo.type === 'scissors') || (choiceTwo.type === 'rock')))	)		
+				 {
+		return choiceOne;
+	}
+	else {
+		return choiceTwo;
+	}
 };
 
 RPS.prototype.RandomChoice = function() {
-	var choices = [new Rock, new Paper, new Scissors]
-	return choices[Math.floor(Math.random() * 3)]
+	var choices = [new Rock, new Paper, new Scissors, new Lizard, new Spock]
+	return choices[Math.floor(Math.random() * 5)]
 };
 
 
