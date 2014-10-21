b47a3fa0a26b103f8e42da6b2fb7221a7238c71d
Denise <yu.denise.d@gmail.com>
2014-10-21 14:37:58 +0100

    Refactored game logic, HTML, CSS

--- src/Rps.js
+++ src/Rps.js
@@ -2,39 +2,49 @@ function RPS() {};
 
 function Rock() {
 	this.type = 'rock';
+	this.message = 'Rock crushes scissors!'
 };
 
 function Paper() {
 	this.type = 'paper';
+	this.message = 'Paper covers rock!';
 };
 
 function Scissors() {
  	this.type = 'scissors';
+ 	this.message = 'Scissors cuts paper!';
  };
 
- function Draw() {};
+ function Draw() {
+ 	this.type = 'draw'
+ 	this.message = 'Draw!'
+ };
 
- rules = new RPS;
+ function PlayerChoice(my_type){
+ 	this.type = my_type
+ };
 
 RPS.prototype.versus = function(choiceOne, choiceTwo) {
  if(choiceOne.type === choiceTwo.type){
- 	return ("It's a draw")
+ 	return (new Draw)
  }
 	if((choiceOne.type === ('rock') && choiceTwo.type === ('scissors')) ||
 	  (choiceOne.type === ('scissors') && choiceTwo.type === ('rock'))) 
-	return ('Rock wins');
+	return (new Rock);
 
 	if((choiceOne.type === ('paper') && choiceTwo.type === ('scissors')) ||
 		(choiceOne.type === ('scissors') && choiceTwo.type === ('paper')))
-	return ('Scissors wins');
+	return (new Scissors);
 
 	if((choiceOne.type === ('paper') && choiceTwo.type === ('rock')) ||
 		(choiceOne.type === ('rock') && choiceTwo.type === ('paper')))
-	return ('Paper wins');
+	return (new Paper);
 };
 
-
-
+RPS.prototype.RandomChoice = function() {
+	var choices = [new Rock, new Paper, new Scissors]
+	return choices[Math.floor(Math.random() * 3)]
+};
 
 
 
--- src/frontEnd.js
+++ src/frontEnd.js
@@ -1,28 +1,18 @@
-		turn = []
-			
-			document.getElementsByTagName('img')[0].onclick = function() { turn.push(new Rock)}
-			document.getElementsByTagName('img')[1].onclick = function() { turn.push(new Paper)}
-			document.getElementsByTagName('img')[2].onclick = function() { turn.push(new Scissors)}
+$(document).ready(function(){
+	var rps = new RPS;
+	var rock = new Rock;
+	var paper = new Paper;
+	var draw = new Draw;
 
-			function displayWinner() {
-				var x = document.createElement("P");
-				var t = document.createTextNode(rules.versus(turn[0], turn[1]));
-   				 x.appendChild(t);
-   				 document.getElementById("myDiv").appendChild(x);
-			}
+	$('img.choice').on('click', function(){
+		var rps = new RPS;
+		var playerchoice = new PlayerChoice($(this).data('pick'))
+		var computerchoice = rps.RandomChoice();
 
-			document.getElementsByTagName('img')[0].addEventListener("click", function() {
-				if (turn.length === 2) {
-				displayWinner()
-				turn = [] }
-			})  
-			document.getElementsByTagName('img')[1].addEventListener("click", function() {
-				if (turn.length === 2) {
-				displayWinner()
-				turn = [] }
-			})
-			document.getElementsByTagName('img')[2].addEventListener("click", function() {
-				if (turn.length === 2) {
-				displayWinner()
-				turn = [] }
-			})
+		$('#player_choice').text('You chose ' + (playerchoice.type));
+		$('#computer_choice').text('Computer chose ' + (computerchoice.type));
+		$('#outcome').text('Result: ' + (rps.versus(playerchoice, computerchoice).message));
+	
+	});
+
+});
